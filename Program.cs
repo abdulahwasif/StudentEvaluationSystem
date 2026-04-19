78dd47534c2d9cd17d30f3bd2699eff4d6ca6db7
abdulahwasif <mrabdullahwasif@gmail.com>
2026-04-19 13:54:49 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using MidDB26.Forms;
+using MidDB26.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MidDB26.Forms
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}
